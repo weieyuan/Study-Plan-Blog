4a9446360a5c7f86374d1fb5a0055249bd24c3c3
weieyuan <weieyuan@yeah.net>
2017-02-07 17:47:08 +0800

    Create AppConfig.java

---
+++ 2017/spring_demo/src/main/java/com/wei/config/AppConfig.java
@@ -0,0 +1,19 @@
+package com.wei.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ImportResource("classpath:/com/wei/config/config.xml") // import xml configruration
+@PropertySource("classpath:/com/wei/config/config.properties") // import source(.properties)
+public class AppConfig {
+
+	@Bean(initMethod="init")
+	public TestBean testBean()
+	{
+		return new TestBean();
+	}
+	
+}
