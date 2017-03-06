18ca27d040c2937aac9dfa251699b4ecededd678
weieyuan <weieyuan@yeah.net>
2017-03-06 22:49:24 +0800

    新增缺失的静态文件

--- 2017/spring_demo/src/main/java/com/wei/HomeController.java
+++ 2017/spring_demo/src/main/java/com/wei/HomeController.java
@@ -2,6 +2,7 @@ package com.wei;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,18 +11,16 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController {
 
 	private static final Logger logger = Logger.getLogger(HomeController.class);
-	
+
 	@PostConstruct
-	public void init()
-	{
+	public void init() {
 		System.out.println("HomeController.init....");
 	}
-	
-	@RequestMapping(value="/home")
-	public String home(ModelAndView m)
-	{
+
+	@RequestMapping(value = "/home")
+	public String home(ModelAndView m) {
 		logger.info("HomeController.home....");
 		return "home";
 	}
-	
+
 }
