7a1bb166df604fbf88e8810178ef4081db12b32f
weieyuan <weieyuan@yeah.net>
2017-03-24 21:48:15 +0800

    新增require.js

--- 2017/spring_demo/src/main/java/com/wei/config/AppConfig.java
+++ 2017/spring_demo/src/main/java/com/wei/config/AppConfig.java
@@ -2,19 +2,25 @@ package com.wei.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
+import com.wei.config.springjpa.SpringJpaConfig;
+
 @Configuration
-@ImportResource("classpath:/com/wei/config/config.xml") // import xml configruration
-@PropertySource("classpath:/com/wei/config/config.properties") // import source(.properties)
+@Import({ SpringJpaConfig.class })
+// import xml configruration
+@ImportResource("classpath:/com/wei/config/config.xml")
+// import source(.properties)
+@PropertySource("classpath:/com/wei/config/config.properties")
 @PropertySource("classpath:config1.properties")
+
 public class AppConfig {
 
-	@Bean(initMethod="init")
-	public TestBean testBean()
-	{
+	@Bean(initMethod = "init")
+	public TestBean testBean() {
 		return new TestBean();
 	}
-	
+
 }
