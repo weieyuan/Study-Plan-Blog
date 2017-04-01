c9d34aaf58c27d8499020beb4e1f00c0c46e97e8
weieyuan <weieyuan@yeah.net>
2017-04-01 18:01:43 +0800

    Create test.js

---
+++ 2017/spring_demo/src/main/webapp/resources/html/test.js
@@ -0,0 +1,56 @@
+/**
+ * Created by Administrator on 2017/4/1.
+ */
+var Test = function(){
+
+    this.init = function(){
+        _initHead();
+        _initContent();
+        _initFoot();
+    };
+
+    var _initHead = function(){
+        var strHtml = '\
+        <nav class="navbar navbar-default navbar-fixed-top">\
+        <div class="container-fluid">\
+            <div class="navbar-header">\
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-6" aria-expanded="false">\
+                    <span class="sr-only">Toggle navigation</span>\
+                    <span class="icon-bar"></span>\
+                    <span class="icon-bar"></span>\
+                    <span class="icon-bar"></span>\
+                </button>\
+                <a class="navbar-brand" href="#">Brand</a>\
+            </div>\
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-6">\
+                <ul class="nav navbar-nav">\
+                    <li class="active"><a href="#">Home</a></li>\
+                    <li><a href="#">Link</a></li>\
+                    <li><a href="#">Link</a></li>\
+                </ul>\
+            </div>\
+        </div>\
+        </nav>';
+        $("#wrapper").prepend(strHtml);
+    };
+
+    var _initContent = function(){
+        var strHtml = '\
+        <div class="container" id="container"></div>';
+
+        $("#wrapper").append(strHtml);
+
+        for(var i = 0; i < 100; i++){
+            strHtml = '<div style="width: 100%; height: 100px; border: 1px solid #d5d5d5; margin: 10px 0 10px 0;">' + i +'</div>';
+            $("#container").append(strHtml);
+        }
+    };
+
+    var _initFoot = function(){
+        var strHtml = '\
+        <div class="panel-footer footer">Panel footer</div>';
+
+        $("#wrapper").append(strHtml);
+    };
+
+};
