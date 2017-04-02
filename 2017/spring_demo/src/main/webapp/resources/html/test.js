0d41d8458f05efbe9ed8628d07c0fbed0a826c60
weieyuan <weieyuan@yeah.net>
2017-04-02 09:57:58 +0800

    更新样式

--- 2017/spring_demo/src/main/webapp/resources/html/test.js
+++ 2017/spring_demo/src/main/webapp/resources/html/test.js
@@ -11,25 +11,7 @@ var Test = function(){
 
     var _initHead = function(){
         var strHtml = '\
-        <nav class="navbar navbar-default navbar-fixed-top">\
-        <div class="container-fluid">\
-            <div class="navbar-header">\
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-6" aria-expanded="false">\
-                    <span class="sr-only">Toggle navigation</span>\
-                    <span class="icon-bar"></span>\
-                    <span class="icon-bar"></span>\
-                    <span class="icon-bar"></span>\
-                </button>\
-                <a class="navbar-brand" href="#">Brand</a>\
-            </div>\
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-6">\
-                <ul class="nav navbar-nav">\
-                    <li class="active"><a href="#">Home</a></li>\
-                    <li><a href="#">Link</a></li>\
-                    <li><a href="#">Link</a></li>\
-                </ul>\
-            </div>\
-        </div>\
+        <nav class="navbar navbar-default navbar-fixed-top custom_nav">\
         </nav>';
         $("#wrapper").prepend(strHtml);
     };
