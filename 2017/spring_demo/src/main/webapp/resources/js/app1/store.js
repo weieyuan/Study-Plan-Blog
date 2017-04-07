c0810b53fb8d1b25f6e7340d798ff06dec942700
weieyuan <weieyuan@yeah.net>
2017-04-07 17:59:06 +0800

    Create store.js

---
+++ 2017/spring_demo/src/main/webapp/resources/js/app1/store.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Administrator on 2017/4/7.
+ */
+define(["app1/mock", "widget/utils"], function(Mock, Utils){
+
+    var Store = new Object();
+    Store.mock = true;
+    Store.getBlogs = function(limit, iPageIndex, oAfterCallback){
+        if(Store.mock){
+            Mock.getBlogs(limit, iPageIndex, oAfterCallback);
+        }
+        else{
+            Utils.postJosn();
+        }
+    };
+
+    Store.getBlogContent = function(blogId, oAfterCallback){
+        if(Store.mock){
+            Mock.getBlogContent(blogId, oAfterCallback);
+        }
+        else{
+            Utils.postJosn();
+        }
+    };
+
+    return Store;
+});
